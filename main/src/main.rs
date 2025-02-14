4b12011e792deddfbe2db20547a34ee3a47c185c
Flambynou <tom.ghislain@ik.me>
2025-02-14 17:11:19 +0100

    right function still bugged but a little better

--- main/src/main.rs
+++ main/src/main.rs
@@ -13,38 +13,38 @@ fn draw_game(game_state : Vec<Vec<i32>>) {
 }
 
 fn move_right(current_state : Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-   
     // Returns the game state after having moved to the right
     // current_state is a square matrix (list of list) of GRID_SIZE dimension
     let mut new_state = current_state.clone();
     for i in 0..GRID_SIZE {         // Loop over every line
-        let mut pointer = 0;         // A pointer for the current right-most free block
+        let mut pointer = GRID_SIZE-1;         // A pointer for the current right-most free block
         let mut noted_value = -1;    // A variable noting the value of the first block encountered to know if the next block can be fused with it
         let mut noted_index:usize= 0;
         let line = &mut new_state[i as usize];
         for j in 0..GRID_SIZE { // Loop over every space in the line
-            if noted_value == -1 && line[j as usize] != 0 { // Change block_value if it's -1 and the value of the current block is not 0
-                noted_value += 1+line[j as usize];
-                noted_index = j as usize
+            let index = (GRID_SIZE - j - 1) as usize;
+            if noted_value == -1 && line[index] != 0 { // Change block_value if it's -1 and the value of the current block is not 0
+                noted_value += 1+line[index];
+                noted_index = index
             }
             else { // Move the noted block at pointer when encountering another block
-                if line[j as usize] != 0 { // If encountering a new block
-                    if line[j as usize] == noted_value { // Fuse them if they have the same value
-                        line[pointer] = noted_value+1;
-                        pointer += 1;
+                if line[index] != 0 { // If encountering a new block
+                    if line[index] == noted_value { // Fuse them if they have the same value
+                        line[pointer as usize] = noted_value+1;
+                        pointer -= 1;
                         noted_value = -1;
                         line[noted_index] = 0;
-                        line[j as usize] = 0
+                        line[index] = 0
                     }
                     else { // Otherwise, move the noted block at pointer and note the new block
-                        line[pointer] = noted_value;
-                        pointer += 1;
+                        line[pointer as usize] = noted_value;
+                        pointer -= 1;
                         line[noted_index] = 0;
-                        noted_value = line[j as usize]
+                        noted_value = line[index]
                     }
                 }
                 else if j == GRID_SIZE-1 { // If at the end of the line, move the noted block at pointer
-                    line[pointer] = noted_value;
+                    line[pointer as usize] = noted_value;
                     line[noted_index] = 0;
                     break
                 }
